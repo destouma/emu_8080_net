97b9f3aff539ee9612429148d1981f8993a15ee2
Manuel DESTOUESSE <manuel.destouesse@itrachealth.com>
2023-01-23 09:53:22 -0500

    add test project + convert in lib

---
+++ Emu8080Tests/Status8080Tests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Emu8080;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emu8080.Tests
+{
+    [TestClass()]
+    public class Status8080Tests
+    {
+        [TestMethod()]
+        public void Status8080Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalcFlagZeroTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalcFlagSignTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalcFlagParityTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalcFlagCarryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalcFlagAuxCarryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalcLogicFlagsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPSWTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetPSWTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
