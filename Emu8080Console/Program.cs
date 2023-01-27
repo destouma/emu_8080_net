15b562549b3ed1859afd150a25c960d809612b72
Manuel DESTOUESSE <manuel.destouesse@itrachealth.com>
2023-01-27 17:05:17 -0500

    init windows forms app

--- Emu8080Console/Program.cs
+++ Emu8080Console/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("8080 CPU Emulator");
 Emulator8080 emulator = new Emulator8080(new ConsoleEmulator8080Listener());
 
 // PC Windows
-//emulator.LoadFileInMemoryAt(0, "C:\\Users\\Manuel DESTOUESSE\\Development\\PERSO\\emu_8080_net\\Emu8080\\invaders.bin");
+emulator.LoadFileInMemoryAt(0, "C:\\Users\\Manuel DESTOUESSE\\Development\\PERSO\\emu_8080_net\\Emu8080\\invaders.bin");
 // Mac
-emulator.LoadFileInMemoryAt(0, "/Users/destouma/Development/PERSO/NETC#/emu_8080_net/Emu8080/invaders.bin");
+//emulator.LoadFileInMemoryAt(0, "/Users/destouma/Development/PERSO/NETC#/emu_8080_net/Emu8080/invaders.bin");
 emulator.Run();
