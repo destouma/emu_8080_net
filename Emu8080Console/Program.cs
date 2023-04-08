40a188d4917b68b081179a9c0f6be89f34b53c03
destouma <mdsoftengr@gmail.com>
2023-04-08 07:51:55 -0400

    WIP merge

--- Emu8080Console/Program.cs
+++ Emu8080Console/Program.cs
@@ -10,13 +10,7 @@ internal class Program
         //Emulator8080 emulator = new Emulator8080(new LiteConsoleEmulator8080Listener());
         Emulator8080 emulator = new Emulator8080(new ConsoleEmulator8080Listener());
 
-<<<<<<< HEAD
-// PC Windows
-//emulator.LoadFileInMemoryAt(0, "C:\\Users\\Manuel DESTOUESSE\\Development\\PERSO\\emu_8080_net\\Emu8080\\invaders.bin");
-// Mac
-emulator.LoadFileInMemoryAt(0, "/Users/destouma/Development/PERSO/EMULATORS/emu_8080_net/invaders.bin");
-emulator.Run();
-=======
+
         // PC Windows
         emulator.LoadFileInMemoryAt(0, "C:\\Users\\Manuel DESTOUESSE\\Development\\PERSO\\emu_8080_net\\Emu8080Library\\invaders.bin");
         // Mac
@@ -24,4 +18,3 @@ emulator.Run();
         emulator.Run();
     }
 }
->>>>>>> 95e375cacdd8f55679b49bb431bd57dfd0f88cd4
